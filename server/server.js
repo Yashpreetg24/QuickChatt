267696f9032b0f877bbc04f45e539b2abdd6f4b9
Yashpreet Gupta <yashpreetg2024@gmail.com>
2026-04-27 19:51:49 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -21,16 +21,16 @@ export const io = new Server(server, {
 export const userSocketMap = {}; // { userId: socketId }
 
 // Socket.io connection handler
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
-    
+    if (userId) userSocketMap[userId] = socket.id;
+
     // Emit online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
@@ -38,12 +38,16 @@ io.on("connection", (socket)=>{
 })
 
 // Middleware setup
-app.use(express.json({limit: "4mb"}));
-app.use(cors());
+app.use(express.json({ limit: "4mb" }));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 
 // Routes setup
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter)
 
